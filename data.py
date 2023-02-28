7895cb9afde7825c1646b8ee6e88c089b0e4985c
Ronak Mehta <rmehta004@gmail.com>
2023-02-28 01:01:16 -0800

    Added data augmentation.

--- data.py
+++ data.py
@@ -2,30 +2,46 @@ import numpy as np
 import torch
 from torchvision.datasets import CIFAR10
 from torch.utils.data import Dataset, DataLoader, RandomSampler
+from torchvision.transforms import Compose, RandomCrop, RandomHorizontalFlip
+
+
+class Cutout:
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, x):
+        x0, y0 = torch.randint(x.shape[-2] - self.size, (1,)), torch.randint(
+            x.shape[-1] - self.size, (1,)
+        )
+        img = x.clone()
+        img[..., y0 : y0 + self.size, x0 : x0 + self.size] = 0.0
+        return img
+
 
 class ImageClassificationDataset(Dataset):
-
     def __init__(self, x, y):
         self.x = torch.tensor(x).float()
         self.y = torch.tensor(y).long()
-        self.sample_size = len(self.x)
+        self.n = len(self.x)
+        self.pipeline = Compose([RandomCrop(32), RandomHorizontalFlip(0.5), Cutout(8)])
 
     def __len__(self):
-        return self.sample_size
+        return self.n
 
     def __getitem__(self, i):
-        return self.x[i], self.y[i]
+        return self.pipeline(self.x[i]), self.y[i]
+
 
 def preprocess(x_tr, x_te):
 
     # Pad
-    # border = 4
-    # x_tr = np.pad(
-    #     x_tr, [(0, 0), (border, border), (border, border), (0, 0)], mode="reflect"
-    # )
-    # x_te = np.pad(
-    #     x_te, [(0, 0), (border, border), (border, border), (0, 0)], mode="reflect"
-    # )
+    border = 4
+    x_tr = np.pad(
+        x_tr, [(0, 0), (border, border), (border, border), (0, 0)], mode="reflect"
+    )
+    x_te = np.pad(
+        x_te, [(0, 0), (border, border), (border, border), (0, 0)], mode="reflect"
+    )
 
     # Normalize.
     mean, std = x_tr.mean(axis=0), x_tr.std(axis=0)
@@ -35,12 +51,6 @@ def preprocess(x_tr, x_te):
     # Transpose to put channels before height and width.
     x_tr = x_tr.transpose(0, 3, 1, 2)
     x_te = x_te.transpose(0, 3, 1, 2)
-
-    # Randomly flip left and write.
-    np.random.seed(123)
-    n = len(x_tr)
-    flips = np.random.binomial(1, 0.5, size=(n))
-    x_tr = np.array([x_tr[i][..., ::-1] if flips[i] else x_tr[i] for i in range(n)])
 
     return x_tr, x_te
 
@@ -60,6 +70,7 @@ def load_cifar10(root="data/"):
     x_train, x_test = preprocess(x_train, x_test)
 
     return x_train, y_train, x_test, y_test
+
 
 def get_cifar10_loaders(batch_size, root="data/"):
     x_train, y_train, x_test, y_test = load_cifar10()
