8bc6473f9434144d9b1f2af0bc6e3dcda89b0b2f
Ronak Mehta <rmehta004@gmail.com>
2024-03-13 01:15:42 -0700

    Create imagenet captions 250k.

--- scripts/zero_shot.sh
+++ scripts/zero_shot.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-model="doubly_centered"
+model="ViT-B-32"
+model_type="open_clip"
 
 cd /mnt/ssd/ronak/output/imagenet_captions_50k/$model
-for dataset in cifar10 cifar100 stl10 caltech101
+for dataset in cifar10 cifar100 stl10
 do
-    clip_benchmark eval --dataset=$dataset --task=zeroshot_classification --pretrained='laion2b_s34b_b79k' --model=$model --model_type=miniclip --output=$dataset.json --batch_size=64
+    clip_benchmark eval --dataset=$dataset --task=zeroshot_classification --pretrained='laion2b_s34b_b79k' --model=$model --model_type=$model_type --output=$dataset.json --batch_size=64
 done                    
